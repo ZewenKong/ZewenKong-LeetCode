701b6d43dd3f57823dbcceefa9a049638b022dda
Zewen Kong <zewen.kong@outlook.com>
2024-04-11 10:42:08 +0800

    240411

---
+++ 0392 - Is Subsequence [E].py
@@ -0,0 +1,13 @@
+def isSubsequence(s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        i = 0
+        j = 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i+=1
+            j+=1
+        return True if i == len(s) else False
